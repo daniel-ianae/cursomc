14bc9b453af7f5ac8bbabbca7afe38f9ac509dc1
Dan Ian <dan.ianae@gmail.com>
2021-01-09 20:52:00 -0200

    Primeiro REST

---
+++ src/main/java/com/gita/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.gita.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@GetMapping("/retrieve")
+	public String retrieve() {
+		return "REST is working";
+	}
+	
+}
