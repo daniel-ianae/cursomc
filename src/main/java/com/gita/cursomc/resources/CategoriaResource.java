da1186533ba43f5a1e5901f5b42c5a100560c9ad
Dan Ian <dan.ianae@gmail.com>
2021-01-09 21:10:09 -0200

    Testando primeira classe de dominio

--- src/main/java/com/gita/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gita/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.gita.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gita.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping("/retrieve")
-	public String retrieve() {
-		return "REST is working";
+	public List<Categoria> retrieve() {
+		
+		Categoria cat1 = new Categoria (1,"informatica");
+		Categoria cat2 = new Categoria (2,"escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 	
 }
