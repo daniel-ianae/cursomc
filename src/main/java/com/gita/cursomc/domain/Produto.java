26ee09f3c9793b7f5c7b7c88bf7797671333bbf4
Dan Ian <dan.ianae@gmail.com>
2021-01-10 10:54:54 -0200

    Adicionando Produto e associacoes

--- src/main/java/com/gita/cursomc/domain/Produto.java
+++ src/main/java/com/gita/cursomc/domain/Produto.java
@@ -11,9 +11,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -21,33 +24,39 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
+	@ManyToMany
+	@JoinTable(name="PRODUTO_CATEGORIA", 
+			   joinColumns = @JoinColumn(name="produto_id"),
+			   inverseJoinColumns = @JoinColumn(name="categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	public Produto() {}
 	
-	public Categoria() {}
-
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
 	@Override
@@ -66,7 +75,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -74,6 +83,7 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
+	
 	
 	
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,19 @@ insert into categoria (id, nome)
 values (1, 'informatica');
 insert into categoria (id, nome)
 values (2, 'escritorio');
+
+insert into produto (id, nome, preco)
+values (1, 'computador', 2000);
+insert into produto (id, nome, preco)
+values (2, 'impressora', 800);
+insert into produto (id, nome, preco)
+values (3, 'mouse', 80);
+
+insert into produto_categoria (produto_id, categoria_id)
+values (1,1);
+insert into produto_categoria (produto_id, categoria_id)
+values (2,1);
+insert into produto_categoria (produto_id, categoria_id)
+values (2,2);
+insert into produto_categoria (produto_id, categoria_id)
+values (3,1);	
