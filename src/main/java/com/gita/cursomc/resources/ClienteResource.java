b88d48120347e3d3fc79aa5b59da000996b229cc
Dan Ian <dan.ianae@gmail.com>
2021-01-10 12:34:37 -0200

    Novo endpoint clientes/{id} disponivel

---
+++ src/main/java/com/gita/cursomc/resources/ClienteResource.java
@@ -0,0 +1,31 @@
+package com.gita.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gita.cursomc.domain.Cliente;
+import com.gita.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> retrieve(@PathVariable Integer id) {
+		
+		Cliente cat1 = clienteService.buscar(id);
+		return ResponseEntity.ok().body(cat1);
+		
+	}
+	
+}
