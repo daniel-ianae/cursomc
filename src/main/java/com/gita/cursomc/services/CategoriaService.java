92b2b1398f7555c3b1787ff66c97cbc33a17422c
Dan Ian <dan.ianae@gmail.com>
2021-01-10 16:45:06 -0200

    Inserindo nova Categoria com POST

--- src/main/java/com/gita/cursomc/services/CategoriaService.java
+++ src/main/java/com/gita/cursomc/services/CategoriaService.java
@@ -14,12 +14,17 @@ import com.gita.cursomc.services.exceptions.ObjectNotFoundException;
 public class CategoriaService {
 	
 	@Autowired
-	private CategoriaRepository categoriaRepo;
+	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = categoriaRepo.findById(id);
+		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
