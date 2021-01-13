f7acb385865a77bfdfd3ad8e17dd38e60541b2df
Dan Ian <dan.ianae@gmail.com>
2021-01-13 16:51:31 -0200

    Incremento de perfil do cliente

--- src/main/java/com/gita/cursomc/services/DBService.java
+++ src/main/java/com/gita/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.gita.cursomc.domain.PagamentoComCartao;
 import com.gita.cursomc.domain.Pedido;
 import com.gita.cursomc.domain.Produto;
 import com.gita.cursomc.domain.enums.EstadoPagamento;
+import com.gita.cursomc.domain.enums.Perfil;
 import com.gita.cursomc.domain.enums.TipoCliente;
 import com.gita.cursomc.repositories.CategoriaRepository;
 import com.gita.cursomc.repositories.CidadeRepository;
@@ -128,13 +129,19 @@ public class DBService {
 		
 		cli1.getTelefones().addAll(Arrays.asList("278788888", "38839288293"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco (null, "Rua flores", "200", "ap 300", "Jardim", "33222", cli1, c1);
 		Endereco e2 = new Endereco (null, "Rua matos", "200", "ap 300", "Jardim", "33222", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepo.saveAll(Arrays.asList(cli1));
-		enderecoRepo.saveAll(Arrays.asList(e1, e2));
+		clienteRepo.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepo.saveAll(Arrays.asList(e1, e2, e3));
 		
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
