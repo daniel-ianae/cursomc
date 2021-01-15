a6a038181c4c38185d8ad7ff9579ab6a85ccf7ff
Dan Ian <dan.ianae@gmail.com>
2021-01-15 18:00:20 -0200

    Refresh token e atualizacao do cliente

--- src/main/java/com/gita/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/gita/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,19 @@
 package com.gita.cursomc.repositories;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gita.cursomc.domain.Cliente;
 import com.gita.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/gita/cursomc/services/PedidoService.java
+++ src/main/java/com/gita/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.gita.cursomc.domain.Cliente;
 import com.gita.cursomc.domain.ItemPedido;
 import com.gita.cursomc.domain.PagamentoComBoleto;
 import com.gita.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.gita.cursomc.domain.enums.EstadoPagamento;
 import com.gita.cursomc.repositories.ItemPedidoRepository;
 import com.gita.cursomc.repositories.PagamentoRepository;
 import com.gita.cursomc.repositories.PedidoRepository;
+import com.gita.cursomc.security.UserSS;
+import com.gita.cursomc.services.exceptions.AuthorizationException;
 import com.gita.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -70,5 +76,17 @@ public class PedidoService {
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 
+	
+	
 }
